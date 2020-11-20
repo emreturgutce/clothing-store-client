9ec676385e8332b6a4b23314011679741f7affde
emreturgutce <emrebursalee@gmail.com>
2020-11-20 18:29:48 +0300

    using navbar comp.

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Navbar from './components/navbar';
 import Home from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={SignUp} />
-    </Switch>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
+    </Container>
   </Router>
 );
 
