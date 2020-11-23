1d8e812101c8b8461fa3bf9cac5487e577868630
emreturgutce <emrebursalee@gmail.com>
2020-11-23 14:34:15 +0300

    using private route and auth reducer

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Container from '@material-ui/core/Container';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
+import PrivateRoute from './components/private-route';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <Container>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-      </Switch>
-    </Container>
-  </Router>
-);
+const App = () => {
+  const isAuth = useSelector((state: any) => state.auth.isAuthenticated);
+
+  return (
+    <Router>
+      <Navbar />
+      <Container>
+        <Switch>
+          <PrivateRoute
+            exact
+            path="/"
+            isAuthenticated={isAuth}
+            Children={Home}
+          />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
