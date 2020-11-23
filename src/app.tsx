1b8ebdda4ed230020b2d216277cb6dfc8f63cbe9
emreturgutce <emrebursalee@gmail.com>
2020-11-23 15:31:41 +0300

    using getUser action

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
 import PrivateRoute from './components/private-route';
+import { getUser, AuthActionTypes } from './actions';
 
 const App = () => {
-  const isAuth = useSelector((state: any) => state.auth.isAuthenticated);
+  const { isAuthenticated: isAuth, isLoading } = useSelector(
+    (state: any) => state.auth,
+  );
+  const dispatch = useDispatch();
+
+  const get = useCallback(() => dispatch(getUser()), [dispatch, getUser]);
+
+  useEffect(() => {
+    dispatch({
+      type: AuthActionTypes.USER_LOADING,
+    });
+    get();
+  }, []);
 
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Switch>
-          <PrivateRoute
-            exact
-            path="/"
-            isAuthenticated={isAuth}
-            Children={Home}
-          />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-        </Switch>
-      </Container>
+      <>
+        <Navbar />
+        {isLoading ? (
+          <div
+            style={{
+              width: '100%',
+              height: '90%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <CircularProgress />
+          </div>
+        ) : (
+          <>
+            <Container>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/"
+                  isAuthenticated={isAuth}
+                  Children={Home}
+                />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={SignUp} />
+              </Switch>
+            </Container>
+          </>
+        )}
+      </>
     </Router>
   );
 };
