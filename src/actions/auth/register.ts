1c73cb23937f7e328fe9b62482afa4dfd50143b1
emreturgutce <emrebursalee@gmail.com>
2020-11-23 14:29:23 +0300

    register action creator implemented

---
+++ src/actions/auth/register.ts
@@ -0,0 +1,49 @@
+import { Dispatch } from 'redux';
+import { gql } from '@apollo/client';
+import { AuthActionTypes } from '../types';
+import { apolloClient } from '../../config/apollo-client';
+
+export const registerUser = (
+  name: string,
+  phone: string,
+  email: string,
+  password: string,
+) => (dispatch: Dispatch) => {
+  apolloClient
+    .mutate({
+      mutation: gql`
+        mutation registerUser($data: RegisterInput) {
+          register(data: $data) {
+            id
+            email
+          }
+        }
+      `,
+      variables: {
+        data: {
+          name,
+          phone,
+          email,
+          password,
+        },
+      },
+    })
+    .then(
+      ({
+        data: {
+          register: { id },
+        },
+      }) => {
+        dispatch({
+          type: AuthActionTypes.REGISTER_SUCCESS,
+          payload: { id },
+        });
+      },
+    )
+    .catch((err) => {
+      console.error(err);
+      dispatch({
+        type: AuthActionTypes.REGISTER_FAIL,
+      });
+    });
+};
