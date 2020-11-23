77f375c00223bddcb920c363d504e27dc4a74297
emreturgutce <emrebursalee@gmail.com>
2020-11-23 15:31:20 +0300

    get-user action creator

--- src/actions/auth/get-user.ts
+++ src/actions/auth/get-user.ts
@@ -1,39 +1,42 @@
 import { Dispatch } from 'redux';
 import { gql } from '@apollo/client';
-import { AuthActionTypes } from '../types';
+import Cookies from 'js-cookie';
+import { AuthActionTypes } from '..';
 import { apolloClient } from '../../config/apollo-client';
 
 export const getUser = () => (dispatch: Dispatch) => {
-  dispatch({
-    type: AuthActionTypes.USER_LOADING,
-  });
+  const cookie = Cookies.get('auth_token');
 
-  apolloClient
-    .query({
-      query: gql`
-        {
-          me {
-            id
-            email
+  if (!cookie) {
+    dispatch({ type: AuthActionTypes.AUTH_FAIL });
+  } else {
+    apolloClient
+      .query({
+        query: gql`
+          {
+            me {
+              id
+              email
+            }
           }
-        }
-      `,
-    })
-    .then(
-      ({
-        data: {
-          register: { id },
+        `,
+      })
+      .then(
+        ({
+          data: {
+            me: { id },
+          },
+        }) => {
+          setTimeout(() => {
+            dispatch({ type: AuthActionTypes.AUTH_SUCCESS, payload: { id } });
+            dispatch({ type: AuthActionTypes.USER_LOADED });
+          }, 1000);
         },
-      }) => {
+      )
+      .catch(() => {
         dispatch({
-          type: AuthActionTypes.USER_LOADED,
+          type: AuthActionTypes.AUTH_FAIL,
         });
-      },
-    )
-    .catch((err) => {
-      console.error(err);
-      dispatch({
-        type: AuthActionTypes.AUTH_FAIL,
       });
-    });
+  }
 };
