03de9fed750d3909df48e0203b2746e5cc9b04c3
emreturgutce <emrebursalee@gmail.com>
2020-11-23 16:02:39 +0300

    name field added to state and action

--- src/reducers/auth/types.ts
+++ src/reducers/auth/types.ts
@@ -2,17 +2,17 @@ import { Action } from 'redux';
 import { AuthActionTypes } from '../../actions';
 
 export interface AuthState {
-  token?: string;
   isAuthenticated: boolean;
   isLoading: boolean;
   user: {
     id?: string;
+    name?: string;
   } | null;
 }
 
 export interface AuthAction extends Action<AuthActionTypes> {
   payload?: {
-    token?: string;
     id?: string;
+    name?: string;
   };
 }
