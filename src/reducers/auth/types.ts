4b5bb7ec630ab68bf6354e0e9edcf27f594b2d71
emreturgutce <emrebursalee@gmail.com>
2020-11-23 14:28:05 +0300

    AuthState and AuthAction interfaces added

---
+++ src/reducers/auth/types.ts
@@ -0,0 +1,18 @@
+import { Action } from 'redux';
+import { AuthActionTypes } from '../../actions/types';
+
+export interface AuthState {
+  token?: string;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: {
+    id?: string;
+  } | null;
+}
+
+export interface AuthAction extends Action<AuthActionTypes> {
+  payload?: {
+    token?: string;
+    id?: string;
+  };
+}
