7acb8a8fbc07299e74098f9755db18873869a5f2
emreturgutce <emrebursalee@gmail.com>
2020-11-23 14:31:32 +0300

    wrapping app with redux provider and apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client';
 import App from './app';
+import { store } from './store';
 import './index.css';
+import { apolloClient } from './config/apollo-client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </Provider>,
+  document.getElementById('app'),
+);
