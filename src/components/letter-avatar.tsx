d9dbe3bc6e7c2ed304c30a30c768d293b22a627b
emreturgutce <emrebursalee@gmail.com>
2020-11-24 12:54:46 +0300

    handling logout

--- src/components/letter-avatar.tsx
+++ src/components/letter-avatar.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import { deepOrange } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { logout } from '../actions';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,7 +20,11 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function LetterAvatar({ firstLetter }: { firstLetter: string }) {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const history = useHistory();
+
+  const handleLout = useCallback(() => dispatch(logout()), [dispatch, logout]);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -25,6 +32,11 @@ export default function LetterAvatar({ firstLetter }: { firstLetter: string }) {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    handleLout();
+    history.push('/');
   };
 
   return (
@@ -41,7 +53,7 @@ export default function LetterAvatar({ firstLetter }: { firstLetter: string }) {
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
       </Menu>
     </div>
   );
