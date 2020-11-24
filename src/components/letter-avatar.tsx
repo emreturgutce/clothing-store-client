80a44e3ffe90e6ec2b93cf30ef8fca0ffd52b0c3
emreturgutce <emrebursalee@gmail.com>
2020-11-24 12:47:07 +0300

    dropdown menu that appears when use click on avatar image

--- src/components/letter-avatar.tsx
+++ src/components/letter-avatar.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import { deepOrange } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,6 +17,32 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function LetterAvatar({ firstLetter }: { firstLetter: string }) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-  return <Avatar className={classes.orange}>{firstLetter}</Avatar>;
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button type="button" onClick={handleClick}>
+        <Avatar className={classes.orange}>{firstLetter}</Avatar>
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
