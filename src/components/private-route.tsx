358af8f1f50d96603eae21f5bd2f9a7c75d70ebd
emreturgutce <emrebursalee@gmail.com>
2020-11-24 14:51:01 +0300

    using global state

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ Children, isAuthenticated, path }: any) => {
+const PrivateRoute = ({ Children, path }: any) => {
+  const { isAuthenticated } = useSelector((state: any) => state.auth);
+
   const routeComponent = (props: any) =>
     isAuthenticated ? <Children /> : <Redirect to={{ pathname: '/login' }} />;
 
