27dcb9719d8a9805585e30754022deffb79094b4
emreturgutce <emrebursalee@gmail.com>
2020-11-25 12:37:22 +0300

    using card component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,81 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Helmet from 'react-helmet';
+import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import Alert from '@material-ui/lab/Alert';
+import Grid from '@material-ui/core/Grid';
+import { getAllProducts } from '../actions';
+import Card from '../components/card';
 
-const Home = () => (
-  <>
-    <Helmet>
-      <title>Clothing Store - Home</title>
-    </Helmet>
-    <div>Homepage</div>
-  </>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(true);
+  const { isLoading, products, errors } = useSelector(
+    (state: any) => state.product,
+  );
 
-export default Home;
+  const getAll = useCallback(() => dispatch(getAllProducts()), [
+    dispatch,
+    getAllProducts,
+  ]);
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>Clothing Store - Home</title>
+      </Helmet>
+      <Container>
+        <Box flexDirection={'column'} pt={3}>
+          {errors && errors.length > 0 && (
+            <Collapse in={open}>
+              <Alert
+                severity="error"
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setOpen(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+              >
+                {errors.join()}
+              </Alert>
+            </Collapse>
+          )}
+          {isLoading ? (
+            <p>Loading</p>
+          ) : (
+            <Box py={3}>
+              <Grid container justify="space-between" spacing={4}>
+                {products.map((product: any) => (
+                  <Grid key={product.id} item>
+                    <Card
+                      key={product.id}
+                      name={product.name}
+                      price={product.price}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          )}
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default React.memo(Home);
