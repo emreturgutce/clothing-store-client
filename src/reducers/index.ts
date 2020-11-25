1302e50b52ef4074cb0ce6009bdd1ac03f331cd7
emreturgutce <emrebursalee@gmail.com>
2020-11-25 12:34:03 +0300

    exporting product reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './auth/auth-reducer';
+import { productReducer } from './product/product-reducer';
 
 export default combineReducers({
   auth: authReducer,
+  product: productReducer,
 });
