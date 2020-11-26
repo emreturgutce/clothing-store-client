1a2c6af3e14b4bd9a4546974a0cd6d30d9c51a08
emreturgutce <emrebursalee@gmail.com>
2020-11-26 17:29:25 +0300

    some changes

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 
 const ProductDetail = () => {
   const productId = useLocation().pathname.split('/')[2];
+  const { products } = useSelector((state: any) => state.product);
+  const product = products.find((p: any) => p.id === productId);
 
   return (
     <>
       <Helmet>
-        <title>Clothing Store - Product Name</title>
+        <title>Clothing Store - {product.name}</title>
       </Helmet>
       <Container>
-        <h1>Detail Page</h1>
+        <h1>{product.name}</h1>
+        <p>{product.price}</p>
       </Container>
     </>
   );
